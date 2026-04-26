92ca734c6dd872c1688035181f86fff49245b859
Dimitris Dafnis <68849116+jim-daf@users.noreply.github.com>
2026-04-26 13:08:42 +0200

    feat(#15): allow disabling WebView hardware acceleration to stop smiley flicker

--- app/src/main/java/com/ayuget/redface/ui/misc/NestedScrollingWebView.java
+++ app/src/main/java/com/ayuget/redface/ui/misc/NestedScrollingWebView.java
@@ -2,8 +2,11 @@ package com.ayuget.redface.ui.misc;
 
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.View;
 import android.webkit.WebView;
 
 import androidx.core.view.NestedScrollingChild;
@@ -21,11 +24,46 @@ public class NestedScrollingWebView extends WebView implements NestedScrollingCh
         this(context, null);
     }
 
+    /**
+     * SharedPreferences key (boolean) used to disable hardware acceleration on
+     * the embedded WebView. See issue #15 for the smiley-induced flickering
+     * behaviour this works around on some devices.
+     */
+    public static final String PREF_DISABLE_HARDWARE_ACCELERATION =
+            "pref_disable_webview_hardware_accel";
+
     public NestedScrollingWebView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         mChildHelper = new NestedScrollingChildHelper(this);
         setNestedScrollingEnabled(true);
+        applyHardwareAccelerationPreference(context);
+    }
+
+    /**
+     * Force the WebView into a software-rendered layer. Useful as a
+     * workaround for hardware-accelerated rendering glitches such as the
+     * flickering text blocks reported when animated smileys are displayed.
+     */
+    public void setSoftwareLayerType() {
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+    }
+
+    /**
+     * Restore the default hardware-accelerated layer type.
+     */
+    public void setHardwareLayerType() {
+        setLayerType(View.LAYER_TYPE_HARDWARE, null);
+    }
+
+    private void applyHardwareAccelerationPreference(Context context) {
+        final SharedPreferences prefs =
+                PreferenceManager.getDefaultSharedPreferences(context);
+        final boolean disableHwAcceleration =
+                prefs.getBoolean(PREF_DISABLE_HARDWARE_ACCELERATION, false);
+        if (disableHwAcceleration) {
+            setSoftwareLayerType();
+        }
     }
 
     @Override
